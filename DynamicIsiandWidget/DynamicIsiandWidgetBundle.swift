9795d7d15a7b6d13510a0a0404fb513a06259459
Cherin <sechan963@gmail.com>
2023-11-08 22:53:29 +0900

    [FEAT] : ⚙️ 다이내믹 아일랜드 구현, 북뷰 한정 사용 예정

---
+++ DynamicIsiandWidget/DynamicIsiandWidgetBundle.swift
@@ -0,0 +1,10 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct DynamicIsiandWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DynamicIsiandWidget()
+        DynamicIsiandWidgetLiveActivity()
+    }
+}
--- histoire/View/Book/BookView.swift
+++ histoire/View/Book/BookView.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 
 struct BookView: View {
+    
+    private var book = Book()
+    
     var body: some View {
         Text("Hello, Book!")
+        Button("start") {
+            book.onLiveActivity()
+        }
+        Button("stop") {
+            book.offLiveActivity()
+        }
     }
 }
 

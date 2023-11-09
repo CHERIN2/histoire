636a81cb1f3f5b2437ba6293318fc75d205d9230
Cherin <sechan963@gmail.com>
2023-11-09 19:42:45 +0900

    [FEAT] : ⚙️ 탭바에 타이틀 다시 넣고 색 추가

--- histoire/Model/Tab/Tab.swift
+++ histoire/Model/Tab/Tab.swift
@@ -7,6 +7,19 @@ enum Tab: String, CaseIterable {
     case chat = "bubble.left.and.text.bubble.right"
     case profiles = "person.crop.circle"
     
+    var title: String {
+           switch self {
+           case .feed:
+               return "Feed"
+           case .book:
+               return "Book"
+           case .chat:
+               return "Chat"
+           case .profiles:
+               return "Profiles"
+           }
+       }
+    
     var index: Int {
         return Tab.allCases.firstIndex(of: self) ?? 0
     }
